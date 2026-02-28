803de97913dcae850ab4ab06f41bf851064e0eed
Sqrcz <naorniakowski@slashlab.pl>
2026-03-01 00:26:10 +0100

    fix: FwbRange thumb sizes fixed and xl size added

--- src/components/FwbRange/composables/useRangeClasses.ts
+++ src/components/FwbRange/composables/useRangeClasses.ts
@@ -4,13 +4,14 @@ import type { InputSize } from '@/components/FwbInput/types'
 
 import { useMergeClasses } from '@/composables/useMergeClasses'
 
-const rangeDefaultClasses = 'w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
+const rangeDefaultClasses = 'fwb-range-input w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
 const rangeLabelClasses = 'block mb-2 text-sm font-medium text-gray-900 dark:text-white'
 
 const rangeSizeClasses: Record<InputSize, string> = {
-  lg: 'h-3 range-lg',
-  md: 'h-2 range-md',
-  sm: 'h-1 range-sm',
+  sm: 'h-1 [&::-webkit-slider-thumb]:size-3 [&::-moz-range-thumb]:size-3',
+  md: 'h-2 [&::-webkit-slider-thumb]:size-4 [&::-moz-range-thumb]:size-4',
+  lg: 'h-3 [&::-webkit-slider-thumb]:size-5 [&::-moz-range-thumb]:size-5',
+  xl: 'h-4 [&::-webkit-slider-thumb]:size-6 [&::-moz-range-thumb]:size-6',
 }
 
 export type UseRangeClassesProps = {
