614380f2a57962cb746e184b76dac6053d0d242b
Sqrcz <naorniakowski@slashlab.pl>
2026-03-01 19:02:32 +0100

    fix: update FwbFileInput button styles for improved accessibility and consistency

--- src/components/FwbFileInput/composables/useFileInputClasses.ts
+++ src/components/FwbFileInput/composables/useFileInputClasses.ts
@@ -7,7 +7,7 @@ import { useMergeClasses } from '@/composables/useMergeClasses'
 const fileInpLabelClasses = 'block mb-2 font-medium text-gray-900 dark:text-white text-sm'
 
 const fileInpDefaultClasses = 'bg-gray-50 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none w-full text-gray-900 dark:text-gray-400 cursor-pointer dark:placeholder-gray-400 placeholder-gray-200'
-const fileInputButtonDefaultClasses = 'hover:file:bg-gray-100 dark:hover:file:bg-gray-600 dark:file:bg-gray-700 file:bg-gray-200 hover:file:border-gray-300 dark:hover:file:border-gray-500 dark:file:border-gray-600 file:border-0 file:border-gray-300 file:border-r file:rounded-none dark:file:text-gray-400 file:text-gray-900 file:cursor-pointer'
+const fileInputButtonDefaultClasses = 'hover:file:bg-gray-100 dark:hover:file:bg-gray-600 dark:file:bg-gray-700 file:bg-gray-200 hover:file:border-gray-300 dark:hover:file:border-gray-500 dark:file:border-gray-600 file:border-0 file:border-gray-300 file:border-r file:rounded-none dark:file:text-white file:text-gray-900 file:transition-colors file:duration-150 file:cursor-pointer'
 
 const fileInpDropzoneClasses = 'flex flex-col justify-center items-center bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 border-2 border-gray-300 dark:border-gray-600 dark:hover:border-gray-500 border-dashed rounded-lg w-full h-64 cursor-pointer'
 const fileDropzoneWrapClasses = 'flex flex-col justify-center items-center pt-5 pb-6'
